40f4e276b9f88130f867f8c9f434b3cc2901e584
patcharavtk <patchara.vtk@gmail.com>
2015-11-06 19:15:33 +0700

    Comment edition.

--- RainyWordServer/src/Main.java
+++ RainyWordServer/src/Main.java
@@ -1,8 +1,5 @@
 import server.SocketServer;
 
-/**
- * Created by peth on 11/5/2015 AD.
- */
 public class Main {
     public static void main(String[] args) {
         SocketServer server = new SocketServer(3322);
